4fb6381a1ea0bc4a2d5e17dc5edf55b2e2cc779e
vkonga <vkongara048@gmail.com>
2026-04-24 12:35:48 +0530

    docker file

--- docker.tf
+++ docker.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "docker" {
   #instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   instance_type = "t3.medium"
+  subnet_id = "subnet-049e9a55205f80cbf"
   # need more for terraform
   root_block_device {
     volume_size = 50
